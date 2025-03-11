8a19bcb5762c8b39d2e783f92bb938215108f5f4
Ran Wei <r.wei@live.co.uk>
2025-03-11 09:12:57 +0000

    .

---
+++ vue-project/Backup/01.own_app/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="app">
+        <h1>hello world!</h1>
+    <!--html-->
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name:'App'
+    }
+</script>
+
+<style>
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ vue-project/src/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="person">
+        <h2>name: {{name}}</h2>
+        <h2>age: {{age}}</h2>
+        <button @click="changeName">Edit Name</button>
+        <button @click="changeAge">Edit Age</button>
+        <button @click="showTel">check contact</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Person',
+        data() {
+            return {
+                name:'Dave',
+                age:18,
+                tel:'1388686111'
+            }
+        },
+        methods:{
+            showTel(){
+                alert(this.tel)
+            },
+            changeName() {
+                this.name="David"
+            },
+            changeAge() {
+                this.age=20
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .person{
+        background-color: #dda;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 5px;
+    }
+</style>
